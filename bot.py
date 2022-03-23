220c6a12e59d9f04d15ee5d3b2a21c2d97a3f930
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2022-03-23 22:02:44 +0000

    Add about command

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import os
 import time
+import datetime
+import platform
 
 import logging
 import discord
@@ -34,7 +36,7 @@ class YayaBot(commands.Bot):
         intents.message_content = True # 0_0
         self.loaded_extensions = []
         self._restart = False
-        super().__init__(command_prefix=self.get_prefix,intents=intents,help_command=yaya.HelpCommand())
+        super().__init__(command_prefix=self.get_prefix, intents=intents, help_command=yaya.HelpCommand())
 
     async def get_prefix(self, message):
         return "-"
@@ -61,6 +63,7 @@ class YayaBot(commands.Bot):
     async def setup_hook(self):
         await self.sql_init()
         await self.load_extensions()
+        self.startTime = time.time()
 
     async def on_ready(self):
         logging.info("Connected!")
@@ -76,9 +79,31 @@ class YayaBot(commands.Bot):
         self._restart = True
         await self.close()
 
+@commands.command(aliases=["info","bot"], brief=":green_book: " )
+async def about(ctx):
+    """Sends some information about the bot!"""
+    currentTime = time.time()
+    uptime = int(round(currentTime - bot.startTime))
+    uptime = str(datetime.timedelta(seconds=uptime))
+    appinfo = await bot.application_info()
+
+    embed = yaya.Embed(ctx.guild.id, bot=bot, description="Yayabot!")
+    embed.set_author(name="YayaBot (Rewrite)", url="https://github.com/JdavisBro/YayaBot-Rewrite/", icon_url=bot.user.avatar.url)
+
+    embed.add_field(name="Instance Owner:", value=appinfo.owner, emoji=":slight_smile:", inline=True)
+    embed.add_field(name="_ _", value="_ _", inline=True)
+    embed.add_field(name="Python Version:", value=f"[{platform.python_version()}](https://www.python.org)", emoji=":snake:", inline=True)
+
+    embed.add_field(name="Bot Uptime:", value=f"{uptime}", emoji=":stopwatch:", inline=True)
+    embed.add_field(name="_ _", value="_ _", inline=True)
+    embed.add_field(name="Discord.py Version:", value=f"[{discord.__version__}](https://github.com/Rapptz/discord.py)", emoji=":desktop:", inline=True)
+
+    await ctx.send(embed=embed)
+
 while True:
     TOKEN = get_token()
     bot = YayaBot()
+    bot.add_command(about)
     bot.run(TOKEN)
     if bot._restart:
         logging.info("Restarting in 3 seconds...")
