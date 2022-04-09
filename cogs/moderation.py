13590c3e152e62aa62a81cc87d83ec6e6c37f4f5
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2022-04-09 16:34:13 +0100

    Ban command added

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -61,3 +61,35 @@ class Moderation(commands.Cog):
         messages = await self.get_purge_messages(ctx.channel, limit, check=check)
         await ctx.channel.delete_messages(messages, reason=f"Purge by {ctx.author} ({ctx.author.id})")
         await ctx.send(f"✅ Purged {len(messages)} messages!", delete_after=2)
+
+    @commands.command(help="Bans the specified `member` for `reason`", brief=":hammer:")
+    #@yaya.checks.is_mod()
+    @commands.has_guild_permissions(ban_members=True) # temporary so randoms don't ban people in the test server lol.
+    async def ban(self, ctx, member: discord.Member, delete_message_days=0, *, reason="No reason specified"):
+        if not ctx.guild.me.guild_permissions.ban_members:
+            await ctx.send("I don't have permission to ban people.")
+            return
+        elif ctx.guild.me.top_role <= member.top_role:
+            await ctx.send("I don't have permission to ban that member.")
+            return
+        # TODO: default ban message per guild
+        userEmbed = yaya.Embed(ctx.guild, bot=self.bot, emoji=":no_entry_sign:", title=f"You have been banned from {ctx.guild.name}", colour=0xff0000)
+        userEmbed.add_field(name="Ban reason: ", value=reason)
+
+        try:
+            await member.send(embed=userEmbed)
+            desc = None
+        except discord.errors.HTTPException:
+            desc = "Failed to send a message to the user."
+
+        if delete_message_days < 0:
+            delete_message_days = 0
+        elif delete_message_days > 7:
+            delete_message_days = 7
+
+        await ctx.guild.ban(member, reason=reason, delete_message_days=delete_message_days)
+
+        channelEmbed = yaya.Embed(ctx.guild, bot=self.bot, emoji=":hammer:", title=f"Banned {str(member)}", colour=0x00ff00, description=desc)
+        await ctx.send(embed=channelEmbed)
+
+        #await yaya.log(self.bot, ctx.guild, ctx.author, member, "ban", reason, datetime.datetime.now())
